6d4574fdb99cd53950254759c2f928d63e9bde40
c <wip>
2024-08-23 17:40:50 -0400

    update

--- demo.sh
+++ demo.sh
@@ -2,3 +2,5 @@ go run entry.go
 go run entry.go job_applications
 go run entry.go _ withsuffix
 go run entry.go job_applications withsuffix
+
+go run entry.go notes # notes/ are .gitignored by default
